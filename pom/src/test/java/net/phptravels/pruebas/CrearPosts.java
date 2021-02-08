1a6c8c50f88a5e6f75bec436ca2550449f8952b3
et58596 <61637059+cmoreraz@users.noreply.github.com>
2021-02-08 13:12:42 -0500

    Update CrearPosts.java

--- pom/src/test/java/net/phptravels/pruebas/CrearPosts.java
+++ pom/src/test/java/net/phptravels/pruebas/CrearPosts.java
@@ -5,13 +5,16 @@ import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
+import net.phptravels.pageobjects.PaginaBlogPosts;
+import net.phptravels.pageobjects.PaginaCrearPosts;
+
 public class CrearPosts extends BaseTestSuite {
-	
+
 	@Test
 	public void login( ) throws InterruptedException {
-		
+
 		googleDriver.get( "https://www.phptravels.net/admin" );
-		
+
 		paginaLogin.iniciarSesion();
 		paginaAdministrador.seleccionarMenu();
 		paginaAdministrador.seleccionarSubMenuCat();
@@ -26,10 +29,14 @@ public class CrearPosts extends BaseTestSuite {
 		paginaBlogPost.clicAdd();
 		Thread.sleep( 5000 );
 		paginaCrearPost.crearPost();
-		Thread.sleep( 5000 );		
 		Thread.sleep( 5000 );
-		
-		
+		paginaCrearPost.numCategoriaCreada();
+		googleDriver.findElement(By.xpath("//*[@id=\"content\"]/form/div[2]/div/div/div[2]/div[2]/div/select/option["+ paginaCrearPost.numCategoriaCreada() + "]")).click();	
+		Thread.sleep( 5000 );	
+		paginaCrearPost.btnGuardar();	
+		Thread.sleep( 5000 );
+
+
 	}
 
 }
