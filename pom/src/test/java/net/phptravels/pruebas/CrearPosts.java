b0ac4c563000f778f0dbcfa89b49297fed01b616
et58596 <61637059+cmoreraz@users.noreply.github.com>
2021-02-09 12:49:10 -0500

    Update CrearPosts.java

--- pom/src/test/java/net/phptravels/pruebas/CrearPosts.java
+++ pom/src/test/java/net/phptravels/pruebas/CrearPosts.java
@@ -1,15 +1,14 @@
 package net.phptravels.pruebas;
 
-import java.util.concurrent.TimeUnit;
-
 import org.junit.Test;
 import org.openqa.selenium.By;
 
-import net.phptravels.pageobjects.PaginaBlogPosts;
-import net.phptravels.pageobjects.PaginaCrearPosts;
-
 public class CrearPosts extends BaseTestSuite {
 
+	/**
+	 * 
+	 * @throws InterruptedException se puede presentar una excepcion en tiempo de ejecucion
+	 */
 	@Test
 	public void login( ) throws InterruptedException {
 
@@ -30,7 +29,10 @@ public class CrearPosts extends BaseTestSuite {
 		Thread.sleep( 5000 );
 		paginaCrearPost.crearPost();
 		Thread.sleep( 5000 );
+
+		//Se recorre la lista de categorias
 		paginaCrearPost.numCategoriaCreada();
+		//Se suma el ultimo numero de la cat y se suma al xpath
 		googleDriver.findElement(By.xpath("//*[@id=\"content\"]/form/div[2]/div/div/div[2]/div[2]/div/select/option["+ paginaCrearPost.numCategoriaCreada() + "]")).click();	
 		Thread.sleep( 5000 );	
 		paginaCrearPost.btnGuardar();	
