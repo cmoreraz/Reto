2bb2379633f3f9901fecdb4071133fb34fe8fd88
et58596 <61637059+cmoreraz@users.noreply.github.com>
2021-02-09 12:49:29 -0500

    Update PaginaBlogCategorias.java

--- pom/src/test/java/net/phptravels/pageobjects/PaginaBlogCategorias.java
+++ pom/src/test/java/net/phptravels/pageobjects/PaginaBlogCategorias.java
@@ -6,19 +6,23 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class PaginaBlogCategorias {
-	
+
+	//Se crean los atributos
+
 	@FindBy( xpath = "//*[@id=\"content\"]/div[2]/div[2]/div[1]/button" )
 	private WebElement btnAdd;
-	
+
 	@FindBy( xpath = "//*[@id=\"content\"]/div[2]/div[2]/div[2]/div/div[1]/div[2]/table/tbody/tr[1]/td[3]" )
 	private WebElement tblCategorias;
-	
-	public void clicBtnAdd( ) {
-		btnAdd.click();
-	}
-	
+
+	//Constructor
 	public PaginaBlogCategorias( WebDriver driver ) {
 		PageFactory.initElements( driver, this );
+	}
+
+	//Metodos personalizados
+	public void clicBtnAdd( ) {
+		btnAdd.click();
 	}
 
 }
